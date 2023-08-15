c9cd39dd34b768e033fd3f4ae923ffc557c8cce0
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-08-15 16:12:25 +0200

    update Tile interface

--- level/Tile.cpp
+++ level/Tile.cpp
@@ -1,1 +1,9 @@
 #include "Tile.h"
+
+Tile::Tile ( char look )
+: m_Look ( look )
+{}
+
+Action Tile::Interact ( void ) {
+    return Action::NOP;
+}
--- level/Tile.h
+++ level/Tile.h
@@ -1,9 +1,12 @@
 #ifndef TILE_H
 #define TILE_H
 
+enum class Action { NOP, STOP };
+
 class Tile {
     public: 
-    
+                       Tile     ( char look );
+        virtual Action Interact ( void );
     private:
         char m_Look;
 };
