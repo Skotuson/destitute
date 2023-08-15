ed8f0a67f10413ae265e7b3f7073311ad677e2ce
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-08-15 13:36:57 +0200

    add level example

--- level/Level.h
+++ level/Level.h
@@ -1,10 +1,16 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#include <string>
+#include <vector>
+
+#include "Tile.h"
+
 class Level {
     public:
-        
+        void Load ( const std::string & filename );
     private:
+        std::vector<std::vector<Tile *>> m_Level;
 };
 
 #endif
