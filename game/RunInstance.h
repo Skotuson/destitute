2003039bf8e469471b1a5f66b0dc60dc364dca4a
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-08-16 12:04:55 +0200

    add Entity class

--- game/RunInstance.h
+++ game/RunInstance.h
@@ -1,10 +1,14 @@
 #ifndef RUNINSTANCE_H
 #define RUNINSTANCE_H
 
+#include "Level.h"
+
 class RunInstance {
     public:
 
     private:
+        Level  m_Level;
+        Entity m_Player;
 };
 
 #endif
