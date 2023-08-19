5f02df7ef090b4bee3707720e74e55bc7eab432d
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-08-19 14:44:07 +0200

    add HandleInteraction
    add m_Player attribute, start HandleInteraction method

--- game/RunInstance.cpp
+++ game/RunInstance.cpp
@@ -6,20 +6,26 @@
 
 RunInstance::RunInstance ( Level * level, const std::vector<Entity *> & entities )
 : m_Level    ( level ),
-  m_Entities ( entities )
+  m_Entities ( entities ),
+  m_Player   ( entities[0] )
 {}
 
 void RunInstance::Run ( void ) {
     while ( true ) {
         std::cout << Draw::RETURN_CURSOR;
         LevelView::View ( *m_Level, m_Entities );
+        std::cout << "Balance: " << m_Balance << std::endl;
         if ( Controller::Peek ( ) ) {
             if ( Controller::Peek ( ) == EXIT_CHAR )
                 break;
             Direction dir = KeyToDirection ( Controller::Get ( ) );
-            Point futureCoords = Translate ( m_Entities[0] -> GetCoords ( ), GetDirectionVector ( dir ) );
+            Point futureCoords = Translate ( m_Player -> GetCoords ( ), GetDirectionVector ( dir ) );
             if ( m_Level -> GetTile ( futureCoords ) -> Interact ( ) != Action::STOP )
-                m_Entities[0] -> Move ( dir );
+                m_Player -> Move ( dir );
         }
     }
+}
+
+void RunInstance::HandleInteraction ( void ) {
+
 }
--- game/RunInstance.h
+++ game/RunInstance.h
@@ -1,19 +1,26 @@
 #ifndef RUNINSTANCE_H
 #define RUNINSTANCE_H
 
+#include <cstdlib>
+
 #include "../level/Level.h"
 #include "../entity/Entity.h"
 
 class RunInstance {
     public:
-             RunInstance ( Level                       * level, 
-                           const std::vector<Entity *> & entities );
+             //First entity will be treated as a player
+             RunInstance       ( Level                       * level, 
+                                 const std::vector<Entity *> & entities );
+      
+        void Run               ( void );
 
-        void Run         ( void );
+        void HandleInteraction ( void );
     private:
+        size_t                  m_Balance = 0;
+
         Level                 * m_Level;
-        Entity                * m_Player;
         std::vector<Entity *>   m_Entities;
+        Entity                * m_Player;
 };
 
 #endif
