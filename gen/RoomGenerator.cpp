9cca5c7b6e51f589dad3bd25291050f3a6f5fd42
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-08-20 23:26:33 +0200

    Update RoomGenerator.cpp

--- gen/RoomGenerator.cpp
+++ gen/RoomGenerator.cpp
@@ -6,13 +6,13 @@
 #include "../level/TileFactory.h"
 
 Room * RoomGenerator::Generate ( void ) {
-    std::vector<std::vector<Tile*>> m_Room;
+    Layout m_Room;
     srand ( time ( nullptr ) );
     size_t n = rand ( ) % MAX_ROOM_SIZE + MIN_ROOM_SIZE;
 
     //Generate layout
     for ( size_t i = 0; i < n; i++ ) {
-        m_Room . push_back ( std::vector<Tile*> ( ) );
+        m_Room . push_back ( std::vector<Tile *> ( ) );
         for ( size_t j = 0; j < n * 2; j++ ) {
             if (    ! j || ! i 
                  || j == ( n * 2 - 1 ) || i == ( n - 1 ) 
