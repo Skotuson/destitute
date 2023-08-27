92c07e07a8e6c218c3b578eaeb72a0460d32d130
Skotuson <58478268+Skotuson@users.noreply.github.com>
2023-08-27 15:33:06 +0200

    update action

--- game/Action.h
+++ game/Action.h
@@ -1,6 +1,6 @@
 #ifndef ACTION_H
 #define ACTION_H
 
-enum class Action { NOP, STOP, LOOT, DOOR };
+enum class Action { NOP, STOP, LOOT, DOOR_UP, DOOR_DOWN, DOOR_LEFT, DOOR_RIGHT };
 
 #endif
--- level/DoorTile.cpp
+++ level/DoorTile.cpp
@@ -10,7 +10,17 @@ DoorTile::~DoorTile ( void )
 }
 
 Action DoorTile::Interact ( void ) {
-    return Action::DOOR;
+    switch ( m_Dir ) {
+        case Direction::UP:
+            return Action::DOOR_UP;
+        case Direction::DOWN:
+            return Action::DOOR_DOWN;
+        case Direction::LEFT:
+            return Action::DOOR_LEFT;
+        case Direction::RIGHT:
+            return Action::DOOR_RIGHT;
+        default: return Action::NOP;
+    }
 }
 
 Direction DoorTile::GetDir ( void ) {
