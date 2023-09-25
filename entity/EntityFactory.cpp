24280870f66b7ba57d16c585457f285f406eec8b
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-09-25 19:07:50 +0200

    update EntityFactory

--- entity/EntityFactory.cpp
+++ entity/EntityFactory.cpp
@@ -3,10 +3,10 @@
 #include "Human.h"
 #include "Projectile.h"
 
-Entity * EntityFactory::Create ( char look, Point coords ) {
+Entity * EntityFactory::Create ( char look, Point coords, Color color, Behavior::BehaviorType behavior ) {
     switch ( look ) {
-        case '&': return new Human ( look, coords );
-        case '*': return new Projectile ( look, coords );
+        case '&': return new Human ( look, coords, color, behavior );
+        case '*': return new Projectile ( look, coords, color, behavior );
         default:
             return nullptr;
     }
--- entity/EntityFactory.h
+++ entity/EntityFactory.h
@@ -2,12 +2,16 @@
 #define ENTITYFACTORY_H
 
 #include "Entity.h"
+#include "Behavior.h"
+#include "../draw/Draw.h"
 #include "../utility/Point.h"
 
 class EntityFactory {
     public:
-        static Entity * Create ( char  look, 
-                                 Point coords );
+        static Entity * Create ( char                   look, 
+                                 Point                  coords,
+                                 Color                  color    = Draw::COLOR_DEFAULT,
+                                 Behavior::BehaviorType behavior = Behavior::BehaviorType::AI );
 };
 
 #endif
